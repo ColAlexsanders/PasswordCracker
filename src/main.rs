7fd71ae5ed6fb8cfb10deeb4b9cfef30acf9b1cc
ColAlexsanders <ahays2357@gmail.com>
2025-09-03 01:37:29 -0500

    9/3/2025
    Added Argon2 dictionary attack functionality.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ use sha2::{Sha256, Digest};
 use bcrypt::verify;
 use std::io::{self, BufRead, Write};
 use std::path::Path;
+use argon2::{
+    password_hash::{ 
+        PasswordHash, PasswordVerifier
+    },
+    Argon2
+};
 //use zip::ZipArchive;
 //use itertools::Itertools;
 
@@ -37,6 +43,10 @@ fn compare_passwords_sha256(word: &str, target_hash: &str) -> bool {
 
 fn compare_passwords_bcrypt(word: &str, target_hash: &str) -> bool {
     verify(word, target_hash).unwrap_or(false)
+}
+
+fn compare_passwords_argon2(word: &str, target_hash: &PasswordHash) -> bool {
+    Argon2::default().verify_password(word.as_bytes(), target_hash).is_ok()
 }
 
 fn read_target_hash_sha256(prompt: &str, blacklist_sha256: &[char]) -> io::Result<String> {
@@ -73,6 +83,18 @@ fn read_target_hash_bcrypt(prompt: &str, blacklist_bcrypt: &[char]) -> io::Resul
     }
 }
 
+fn read_target_hash_argon2(prompt: &str) -> io::Result<String> {
+    loop{
+        print!("{}", prompt);
+        io::stdout().flush()?;
+        let mut input = String::new(); 
+        io::stdin().read_line(&mut input)?;
+        let input = input.trim().to_string();
+        
+        return Ok(input);
+    }
+}
+
 fn has_blacklisted_chars(input: &str, blacklist: &[char]) -> bool {
     for c in input.chars() {
         if blacklist.contains(&c) {
@@ -90,6 +112,7 @@ fn display_menu() -> io::Result<usize> {
     println!("Select an option:");
     println!("1. Crack SHA256 hash");
     println!("2. Crack bcrypt hash");
+    println!("3. Crack argon2 hash");
 /*    println!("3. Crack zip file with dictionary attack");
     println!("4. Crack zip file with brute force attack"); */
     println!("100. Exit Program");
@@ -102,9 +125,9 @@ fn display_menu() -> io::Result<usize> {
             println!("Exiting program...");
             std::process::exit(0);
         },
-        Ok(choice) if choice >= 1 && choice <= /*4*/ 2 => Ok(choice),
+        Ok(choice) if choice >= 1 && choice <= 3 => Ok(choice),
         _ => {
-            eprintln!("Invalid choice. Please enter a number between 1 and 2 or the exit option.");
+            eprintln!("invalid choice. please enter a number between 1 and 3 or the exit option.");
             display_menu()
         }
     }
@@ -141,98 +164,130 @@ fn display_menu() -> io::Result<usize> {
 
 /* ---------------------------------------- MAIN FUNCTION --------------------------------------- */
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file_path = Path::new("rockyou.txt");
-    let blacklist_sha256 = vec!['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', ':', ';', '|', '"', ',', '.', '/', '~', '`', '>', '<', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    let blacklist_bcrypt = vec!['!', '@', '#', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', ':', ';', '|', '"', ',', '~', '`', '>', '<', '?'];
-    let choice = display_menu()?;
-    let target_hash: String;
-    let passwords: Vec<String>;
-    
-    match choice {
-        1 => {
-            target_hash = read_target_hash_sha256("Enter target hash: ", &blacklist_sha256)?;
-            passwords = match read_from_list(file_path) {
-                Ok(p) => p,
-                Err(e) => {
-                    eprintln!("Error reading wordlist: {}", e);
-                    return Ok(());
-                }
-            };
-        }    
-        2 => {
-            target_hash = read_target_hash_bcrypt("Enter target hash: ", &blacklist_bcrypt)?;
-            passwords = match read_from_list(file_path) {
-                Ok(p) => p,
-                Err(e) => {
-                    eprintln!("Error reading wordlist: {}", e);
-                    return Ok(());
-                }
-            };
-        }
-        _ => {
-            eprintln!("Invalid choice");
-            return Ok(());
-        }
-    }
-    
-    // Now set up threading and processing (outside of the match statement)
-    let start_time = Instant::now();
-    let num_threads = 8;
-    let chunk_size = passwords.len() / num_threads;
-    let found_password = Arc::new(AtomicBool::new(false));
-    let mut handles = vec![];
-    
-    for i in 0..num_threads {
-        let start = i * chunk_size;
-        let end = if i == num_threads - 1 { 
-            passwords.len() 
-        } else { 
-            start + chunk_size 
-        };
+fn main() -> Result<(), Box<dyn std::error::Error>> {    
+    loop{
+        let file_path = Path::new("rockyou.txt");
+        let blacklist_sha256 = vec!['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', ':', ';', '|', '"', ',', '.', '/', '~', '`', '>', '<', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        let blacklist_bcrypt = vec!['!', '@', '#', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', ':', ';', '|', '"', ',', '~', '`', '>', '<', '?'];
+        let choice = display_menu()?;
+        let target_hash: String;
+        let passwords: Vec<String>;
         
-        let words = passwords[start..end].to_vec();
-        let target_hash_clone = target_hash.clone();
-        let choice_clone = choice;
-        let found_password_clone = Arc::clone(&found_password);
-        
-        let handle = thread::spawn(move || {
-            for word in &words {
-                if found_password_clone.load(Ordering::SeqCst) {
-                    break;
-                }
-                
-                match choice_clone {
-                    1 => {
-                        if compare_passwords_sha256(word, &target_hash_clone) {
-                            println!("Found password: {}", word);
-                            found_password_clone.store(true, Ordering::SeqCst);
-                        }
+        match choice {
+            1 => {
+                target_hash = read_target_hash_sha256("Enter target hash: ", &blacklist_sha256)?;
+                passwords = match read_from_list(file_path) {
+                    Ok(p) => p,
+                    Err(e) => {
+                        eprintln!("Error reading wordlist: {}", e);
+                        return Ok(());
                     }
-                    2 => {
-                        if compare_passwords_bcrypt(word, &target_hash_clone) {
-                            println!("Found password: {}", word);
-                            found_password_clone.store(true, Ordering::SeqCst);
-                        }
+                };
+            }    
+            2 => {
+                target_hash = read_target_hash_bcrypt("Enter target hash: ", &blacklist_bcrypt)?;
+                passwords = match read_from_list(file_path) {
+                    Ok(p) => p,
+                    Err(e) => {
+                        eprintln!("Error reading wordlist: {}", e);
+                        return Ok(());
                     }
-                    _ => unreachable!(),
-                }
+                };
             }
-        });
+            3 => {
+                target_hash = read_target_hash_argon2("Enter target hash:")?;
+                passwords = match read_from_list(file_path) {
+                    Ok(p) => p,
+                    Err(e) => {
+                        eprintln!("Error reading wordlist: {}", e);
+                        return Ok(());
+                    }
+                };
+            }
+            _ => {
+                eprintln!("Invalid choice");
+                return Ok(());
+            }
+        }
+
+        let found_password = Arc::new(AtomicBool::new(false));
+        let start_time = Instant::now();
+        let num_threads = 8;
+        let chunk_size = passwords.len() / num_threads;
+        let mut handles = vec![];
+
+        for i in 0..num_threads {
+            let start = i * chunk_size;
+            let end = if i == num_threads - 1 { 
+                passwords.len() 
+            } else { 
+                start + chunk_size 
+            };
+            
+            let words = passwords[start..end].to_vec();
+            let target_hash_clone = target_hash.clone();
+            let choice_clone = choice;
+            let found_password_clone = Arc::clone(&found_password);
+            
+            let handle = thread::spawn(move || {
+                let target_hash_parsed_argon2= if choice_clone== 3 {
+                    match PasswordHash::new(&target_hash_clone) {
+                        Ok(parsed) => {
+                            Some(parsed)
+                        },
+                        Err(e) => {
+                            println!("Thread {}: Invalid Argon2 hash format: {}", i, e);
+                            return;
+                        }
+                    }
+                } else {
+                    None
+                };
+                
+                for word in &words {
+                    if found_password_clone.load(Ordering::SeqCst) {
+                        break;
+                    }
+                    
+                    match choice_clone {
+                        1 => {                 
+                            if compare_passwords_sha256(word, &target_hash_clone) {
+                                println!("Found password: {}", word);
+                                found_password_clone.store(true, Ordering::SeqCst);
+                            }
+                        }
+                        2 => {
+                            if compare_passwords_bcrypt(word, &target_hash_clone) {
+                                println!("Found password: {}", word);
+                                found_password_clone.store(true, Ordering::SeqCst);
+                            }
+                        }
+                        3 => {
+                            if let Some(ref parsed_hash)= target_hash_parsed_argon2 {
+                                 if compare_passwords_argon2(word, parsed_hash) {
+                                    println!("Found password: {}", word);
+                                    found_password_clone.store(true, Ordering::SeqCst);
+                                }
+                            }
+                        }                        
+                        _ => unreachable!(),
+                    }
+                }
+            });  
+            handles.push(handle);
+        }
         
-        handles.push(handle);
+        for handle in handles {
+            handle.join().unwrap();
+        }
+       
+        std::thread::sleep(std::time::Duration::from_millis(100)); 
+
+        let elapsed = start_time.elapsed();
+        println!("Time taken: {:.2?}", elapsed);
     }
-    
-    for handle in handles {
-        handle.join().unwrap();
-    }
-    
-    let elapsed = start_time.elapsed();
-    println!("Time taken: {:.2?}", elapsed);
-    
-    Ok(())
 }   
-        /*3 | 4 => {
+    /*3 | 4 => {
             print!("Please enter the path to the zip file: ");
             io::stdout().flush()?;
             let mut input = String::new();
